70ec4df7c3406566fc14c8e0fd5b0b2e6c216616
Beverly Pham <beverly.pham@gmail.com>
2018-03-20 19:42:08 -0700

    about to test

--- test_snakes_cafe.py
+++ test_snakes_cafe.py
@@ -1,18 +1,19 @@
 import pytest
+import snakes_cafe
 
 def test_print_menu():
-    assert snakes_cafe.print_menu
+    assert print_menu
 
 def test_print_order():
-    assert snakes_cafe.print_order
+    assert print_order
 
 def test_remove_item():
-    assert snakes_cafe.remove_item
+    assert remove_item
 
 def test_print_category():
-    assert snakes_cafe.print_category
+    assert print_category
 
 def test_add_to_order():
-    assert snakes_cafe.add_to_order
+    assert add_to_order
 
 # def test_input_item - not needed at this time, relies on user input
